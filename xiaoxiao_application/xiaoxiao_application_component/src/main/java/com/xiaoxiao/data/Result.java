acda2a7b22dd863ef26e280c2dadffc9909e7744
guoqi <guoqi76327@163.com>
2019-10-30 17:25:44 +0800

    xiaoxiao项目服务 修改了组件 添加result结果

---
+++ xiaoxiao_application/xiaoxiao_application_component/src/main/java/com/xiaoxiao/data/Result.java
@@ -0,0 +1,93 @@
+package com.xiaoxiao.data;
+
+/**
+ * @project_name:xiaoxiao_application
+ * @date:2019/10/30:17:20
+ * @author:shinelon
+ * @Describe:
+ */
+
+public class Result
+{
+    /**
+     * 消息
+     */
+    private String msg;
+
+
+    /**
+     * 是否成功
+     */
+    private boolean succeed;
+
+
+    /**
+     * 返回的数据
+     */
+    private Object data;
+
+
+    public String getMsg()
+    {
+        return msg;
+    }
+
+    public void setMsg(String msg)
+    {
+        this.msg = msg;
+    }
+
+    public boolean isSucceed()
+    {
+        return succeed;
+    }
+
+    public void setSucceed(boolean succeed)
+    {
+        this.succeed = succeed;
+    }
+
+    public Object getData()
+    {
+        return data;
+    }
+
+    public void setData(Object data)
+    {
+        this.data = data;
+    }
+
+
+    /**
+     * 成功消息
+     * @param msg
+     * @param data
+     * @return
+     */
+    public static Result succeed(String msg,Object data){
+        return new Result(msg,data);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Result{" + "msg='" + msg + '\'' + ", succeed=" + succeed + ", data=" + data + '}';
+    }
+
+    public Result(String msg, Object data)
+    {
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public Result(String msg, boolean succeed, Object data)
+    {
+        this.msg = msg;
+        this.succeed = succeed;
+        this.data = data;
+    }
+
+    public Result()
+    {
+    }
+}
---
+++ xiaoxiao_application/xiaoxiao_application_component/src/main/java/com/xiaoxiao/utils/IDUtils.java
@@ -0,0 +1,11 @@
+package com.xiaoxiao.utils;
+
+/**
+ * @project_name:xiaoxiao_application
+ * @date:2019/10/30:17:20
+ * @author:shinelon
+ * @Describe:
+ */
+public class IDUtils
+{
+}
